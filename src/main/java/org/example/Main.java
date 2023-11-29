fdd944f386e8e5aac51778a310f33f5fd199b486
denisovamaria <mvdenisova.work@gmail.com>
2023-11-29 20:30:38 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class Main {
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "3Dv5TaFr";
+    public static final String URL = "jdbc:mysql://localhost:3306/myshop";
+    public static Statement statement;
+    public static Connection connection;
+    public static void main(String[] args) {
+
+    }
+}
